9e0c6e3add85512f71e2eca3bfefe885ab19b3b4
Angel Devil <97808852+AngelDevil1223@users.noreply.github.com>
2022-03-13 19:01:43 +0300

    init

---
+++ ThorApps/SiteSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThorApps
+{
+    public static class SiteSettings
+    {
+        public static string PhoneNo => @"+0011 123 456";
+        public static string Email => @"info@thorapps.com";
+        public static string Facebook => @"https://facebook.com/thrapps";
+        public static string Twitter => @"https://twitter.com/@thrapps";
+        public static string Linkedin => @"https://linkedin.com/thrapps";
+    }
+}
