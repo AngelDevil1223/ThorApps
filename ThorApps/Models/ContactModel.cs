5fabd3ef6845ff36e114332f81526ecb186a85b6
AngelDevil1223 <twintwo815@gmail.com>
2022-03-20 06:20:52 +0300

    fix : v2.0

---
+++ ThorApps/Models/ContactModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ThorApps.Models
+{
+    public class ContactModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Required,EmailAddress(ErrorMessage ="Enter a valid email address")]
+        public string Email { get; set; }
+        public string JobTitle { get; set; }
+        public string Phone { get; set; }
+        public string Company { get; set; }
+        public string Country { get; set; }
+        [Required(ErrorMessage ="Type your message")]
+        [MinLength(150,ErrorMessage ="Type at least 150 characters")]
+        public string Message { get; set; }
+    }
+}
